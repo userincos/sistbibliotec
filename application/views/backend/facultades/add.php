9a1c4e14894b6a6337769b916aec966a2e626380
userincos <tiwanakunocturno@gmail.com>
2025-09-17 01:35:45 -0400

    cambios realizados en el titulo de las areas de estudio dentro de la carrera de ingenieria industrial que pertenecen a las areas donde se realizan los proyectos de grados y trabajos dirigidos relacionados con ingenieria industrial

--- application/views/backend/facultades/add.php
+++ application/views/backend/facultades/add.php
@@ -3,7 +3,7 @@
       <!-- Default box -->
     <div class="box">
         <div class="box-header with-border">
-            <h3 class="box-title">Registrar Carrera</h3>
+            <h3 class="box-title">Registrar Área de Estudio</h3>
 
             <div class="box-tools pull-right">
                 <button type="button" class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip" title="Collapse">
